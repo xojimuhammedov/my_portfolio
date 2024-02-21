fad2ba21e02e91dc538389c5f29cee01b44eb968
Otabek Abdiraimov <abdiraimov032@gmail.com>
2024-02-21 16:42:53 +0500

    new blog is added

--- components/cards/blog.tsx
+++ components/cards/blog.tsx
@@ -20,13 +20,13 @@ function BlogCard(blog: Props) {
 		>
 			<Link href={`/blogs/${blog.slug}`}>
 				<div>
-					<div className='relative bg-secondary rounded-md'>
+					<div className='relative bg-secondary rounded-md !max-w-[650px] !max-h-[335px]'>
 						<Image
 							width={650}
 							height={335}
 							src={blog.image?.url}
 							alt={blog.title}
-							className='px-2 max-w-[450px] max-h-[335px] md:px-7 rounded-md group-hover:-translate-y-7 -translate-y-6 transition-all object-cover grayscale group-hover:grayscale-0 max-md:-translate-y-2 max-md:group-hover:-translate-y-3'
+							className='px-2 !max-w-[550px] !max-h-[250px] md:px-7 !rounded-xl group-hover:-translate-y-7 -translate-y-6 transition-all object-cover grayscale group-hover:grayscale-0 max-md:-translate-y-2 max-md:group-hover:-translate-y-3'
 						/>
 					</div>
 				</div>
