26d006b83a5bd25164106f1335210e9247b145bd
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-08-06 21:15:42 +0500

    add

--- app/layout.tsx
+++ app/layout.tsx
@@ -20,26 +20,26 @@ const workSans = Work_Sans({
 });
 
 export const metadata: Metadata = {
-  metadataBase: new URL("https://otabekdev-blog.vercel.app"),
-  title: "OtabekDev — articles about programming",
+  metadataBase: new URL("https://www.muhammadislom.uz/"),
+  title: "Muhammadislom — articles about programming",
   description:
-    "OtabekDev offers a variety of insightful articles covering programming topics, ranging from fundamental concepts to advanced techniques. Explore practical tutorials, insightful discussions, and expert tips to enhance your programming skills.",
+    "Muhammadislom offers a variety of insightful articles covering programming topics, ranging from fundamental concepts to advanced techniques. Explore practical tutorials, insightful discussions, and expert tips to enhance your programming skills.",
   authors: [
-    { name: "Otabek Abdiraimov", url: "https://otabekdev-blog.vercel.app" },
+    { name: "Muhammadislom Hojimuhammedov", url: "https://www.muhammadislom.uz/" },
   ],
-  icons: { icon: "/favicon.png" },
+  // icons: { icon: "/favicon.png" },
   keywords:
-    "otabekdev, programming articles, programming language hacks, programming language tricks, python hacks and tricks, javaScript hacks and tricks, java hacks and tricks, c++ hacks and tricks, web development tips, software engineering insights, coding tutorials and guides, frontend development techniques, backend development strategies, algorithm optimization tricks, data structure hacks, framework comparisons, debugging secrets, version control mastery, agile development methodologies, Tech industry insights, articles about programming, otabekdev articles",
+    "Muhammadislom, programming articles, programming language hacks, programming language tricks, python hacks and tricks, javaScript hacks and tricks, java hacks and tricks, c++ hacks and tricks, web development tips, software engineering insights, coding tutorials and guides, frontend development techniques, backend development strategies, algorithm optimization tricks, data structure hacks, framework comparisons, debugging secrets, version control mastery, agile development methodologies, Tech industry insights, articles about programming, otabekdev articles",
   openGraph: {
-    title: "OtabekDev — articles about programming",
+    title: "Muhammadislom — articles about programming",
     description:
-      "OtabekDev offers a variety of insightful articles covering programming topics, ranging from fundamental concepts to advanced techniques. Explore practical tutorials, insightful discussions, and expert tips to enhance your programming skills",
+      "Muhammadislom offers a variety of insightful articles covering programming topics, ranging from fundamental concepts to advanced techniques. Explore practical tutorials, insightful discussions, and expert tips to enhance your programming skills",
     type: "website",
-    url: "https://otabekdev-blog.vercel.app",
+    url: "https://www.muhammadislom.uz/",
     locale: "en_EN",
-    images: "/OtabekDev.png",
+    // images: "/favicon.png",
     countryName: "America",
-    siteName: "OtabekDev",
+    siteName: "Muhammadislom",
     emails: "abdiraimov032@gmail.com",
   },
 };
