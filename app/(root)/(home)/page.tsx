40599d083f1632c8b0b623a5826cedd66adf7645
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-08-07 15:17:10 +0500

    projects add

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -1,12 +1,19 @@
 import BlogCard from "@/components/cards/blog";
 import BgArrow from "@/components/shared/bgArrow";
+import Image from "next/image";
+
+import BlogImage from "../../../public/blogs/blog.jpg";
+import AlStarImage from "../../../public/about/04.jpg";
+import SerEnergies from "../../../public/about/05.jpg";
+import AssiTech from "../../../public/about/07.jpg";
+import SamTrip from "../../../public/about/09.jpg";
 
 async function HomePage() {
   return (
     <div className="max-w-3xl md:max-w-6xl mx-auto">
       <div className="relative min-h-[60vh] flex items-center justify-center">
         <h1 className="text-3xl md:text-4xl lg:text-5xl font-creteRound text-bold text-center max-w-2xl">
-          Taking control of your daily life is easy when know how!
+          Create and develop a website for your business with us!
         </h1>
         <div className="hidden sm:flex">
           <BgArrow />
@@ -16,11 +23,48 @@ async function HomePage() {
         <span>Recent Posts</span>
       </h2>
       <div className="flex flex-col space-y-24 mt-24">
-        <BlogCard />
+        <BlogCard
+          img={BlogImage}
+          title={"Create Business Website"}
+          pathname={"/"}
+          subtitle={"We create busines website for you"}
+        />
       </div>
       <h2 className="text-center text-4xl mt-24 section-title font-creteRound">
         <span>Our Projects</span>
       </h2>
+      <div className="flex flex-col space-y-24 mt-24">
+        <BlogCard
+          img={AlStarImage}
+          title={"ALSTAR Website"}
+          pathname={"http://alstar.uz/"}
+          subtitle={
+            "ALSTAR is a brand of PE, FR-B1, FR-A2 type Aluminum Composite Panel (ACP) owned by “GOLDEN HAPPINESS” LLC"
+          }
+        />
+        <BlogCard
+          img={SerEnergies}
+          title={"SER ENERGIES GROUP Website"}
+          pathname={"http://serenergies.com/"}
+          subtitle={
+            "SER ENERGIES GROUP Website is Drawing construction project according the main project design."
+          }
+        />
+        <BlogCard
+          img={AssiTech}
+          title={"ASSITECHBIO Website"}
+          pathname={"https://assitech.uz/"}
+          subtitle={
+            "Our company ASSITECHBIO specializes in: sales and service of ultrasound devices and organization of ultrasound courses."
+          }
+        />
+        <BlogCard
+          img={SamTrip}
+          title={"Samandar Trip Tour"}
+          subtitle={"Samandar Trip Tour is support to get Uzbekistan visa."}
+          pathname={"https://samtrip.uz/"}
+        />
+      </div>
     </div>
   );
 }
--- components/cards/blog.tsx
+++ components/cards/blog.tsx
@@ -4,23 +4,20 @@ import { CalendarDays, Clock, Dot, Layers2, Minus, Tag } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
 import { Badge } from "../ui/badge";
-import BlogImage from '../../public/blogs/blog.jpg'
-import AvatarIcon from '../../public/blogs/123.jpg';
+import BlogImage from "../../public/blogs/blog.jpg";
+import AvatarIcon from "../../public/blogs/123.jpg";
 
-function BlogCard() {
+function BlogCard({ img, title, subtitle, pathname }: any) {
   return (
-    <div
-      className={cn(
-        "grid gap-4 group grid-cols-1 md:grid-cols-2",
-      )}>
+    <div className={cn("grid gap-4 group grid-cols-1 md:grid-cols-2")}>
       <Link href={`/`}>
         <div>
           <div className="relative bg-secondary rounded-md !max-w-[650px] !max-h-[335px]">
             <Image
               width={650}
               height={335}
-              src={BlogImage}
-              alt={'BlogImage'}
+              src={img}
+              alt={"BlogImage"}
               className="px-2 !max-w-[250px] md:!max-w-[550px] md:!max-h-[380px] md:px-7 !rounded-xl group-hover:-translate-y-7 -translate-y-6 transition-all object-cover grayscale group-hover:grayscale-0 max-md:-translate-y-2 max-md:group-hover:-translate-y-3"
             />
           </div>
@@ -41,11 +38,9 @@ function BlogCard() {
           </div>
 
           <h2 className="text-3xl max-md:text-2xl font-creteRound group-hover:text-blue-500 transition-colors">
-            Create Business Website
+            {title}
           </h2>
-          <p className="text-muted-foreground line-clamp-3">
-            We create busines website for you
-          </p>
+          <p className="text-muted-foreground line-clamp-3">{subtitle}</p>
         </Link>
 
         {/* Author */}
@@ -65,18 +60,27 @@ function BlogCard() {
             <Link href={`/`}>
               <Badge variant={"secondary"} role="button">
                 <Tag className="w-3 h-3 me-2" />
-                 busines
+                busines
               </Badge>
             </Link>
             <Dot />
             <Link href={`/`}>
               <Badge variant={"outline"} role="button">
                 <Layers2 className="w-3 h-3 me-2" />
-                 seo
+                seo
               </Badge>
             </Link>
           </div>
         </div>
+          <Link
+            href={pathname}
+            target="_blank"
+            className={cn(
+              "hover:bg-blue-400/20 py-1 px-3 w-[108px] cursor-pointer rounded-sm transition-colors"
+              // pathname === nav.route && "text-blue-400"
+            )}>
+            Read More
+          </Link>
       </div>
     </div>
   );
