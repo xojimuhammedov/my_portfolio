ec0320c6cdfef77cd50f7659b57160f1f7132060
muhammadislom2004 <muhammadislomxojimuhammedov0@gmail.com>
2024-08-07 10:45:06 +0500

    add

--- app/(root)/_components/Navbar.tsx
+++ app/(root)/_components/Navbar.tsx
@@ -14,7 +14,7 @@ function Navbar() {
     <div className="h-[10vh] backdrop-blur-sm border-b fixed z-40 inset-0 bg-background">
       <div className="container max-w-3xl md:max-w-6xl mx-auto h-[10vh] w-full flex items-center justify-between">
         <Link href={"/"}>
-          <h1 className="text-4xl font-creteRound">MuhammadislomDev</h1>
+          <h1 className="text-2xl font-creteRound">MuhammadislomDev</h1>
         </Link>
         {/* Nav links */}
         <div className="gap-2 hidden md:flex">
